48f6de331641a33a60a0d48d64a72a98b94e698d
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2019-07-30 11:33:26 +0300

    deleted unrelated code snippet for another thing
    used here as a darft for a solution to a colleagues issue, lame. forgot to delete

--- range-array-from.js
+++ range-array-from.js
@@ -1,14 +1,1 @@
 let range = Array.from(new Array(r - l + 1)).map((_, i) => l + i)
-
-
-    // Write your code here
-  let a = tickets[p];
-  let smallers = tickets.filter(x => x < a);
-  let s_sum = smallers.reduce((acc, x) => acc + x, 0);
-  let l_biggers = tickets.filter((x, i) => x >= a && i < p);
-  // let l_biggers_sum = l_biggers.reduce((acc, x) => acc + x, 0);
-  let r_biggers = tickets.filter((x, i) => x >= a && i < p);
-  // let r_biggers_sum = r_biggers.reduce((acc, x) => acc + x, 0);
-
-  let alex_time = (s_sum + (a * l_biggers.length) + (a * r_biggers.length) + a);
-  return alex_time;
