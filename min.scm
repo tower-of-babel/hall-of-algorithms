ab0a7c7baa1e1ec40ab4e205884c86cc21f6b791
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2019-08-09 11:29:11 +0300

    Update min.scm

--- min.scm
+++ min.scm
@@ -1,4 +1,4 @@
-    
+;; takes a list of atoms and returns the minimum one
 (define min
   (lambda (lat)
     (cond
@@ -8,3 +8,4 @@
           ((null? (cdr lat)) (car lat))
           ((< (car lat) (car (cdr lat))) (min (cons (car lat) (cdr (cdr lat)))))
           (else (min (cdr lat))))))))
+; (min '(3 4 -10 0 -1)) returns -10
