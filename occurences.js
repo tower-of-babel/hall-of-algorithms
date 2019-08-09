09395c4bee8feeb5f848c3baab436a7f802d14c1
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2019-08-09 11:18:33 +0300

    Update occurences.js

--- occurences.js
+++ occurences.js
@@ -1,11 +1,12 @@
-function occur(elt, seq) {
+// natural recursive
+function occurences(elt, seq) {
     if(seq.length === 0) {
         return 0;
     } else {
         if (elt === seq[0]) {
-            return 1 + occur(elt, seq.slice(1));
+            return 1 + occurences(elt, seq.slice(1));
         } else {
-            return occur(elt, seq.slice(1));
+            return occurences(elt, seq.slice(1));
         }
     }
 }
