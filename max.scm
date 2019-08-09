b282b3e9f0619d55fcd266c6d9520d4c30cf8e7e
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2019-08-09 11:07:21 +0300

    Update max.scm

--- max.scm
+++ max.scm
@@ -2,6 +2,8 @@
   (lambda (lat)
     (cond
       ((null? lat) (quote ()))
-      ((null? (cdr lat)) (car lat))
-      ((> (car lat) (car (cdr lat))) (max (cons (car lat) (cdr (cdr lat)))))
-      (else (max (cdr lat))))))
+      (else
+       (cond
+        ((null? (cdr lat)) (car lat))
+        ((> (car lat) (car (cdr lat))) (max (cons (car lat) (cdr (cdr lat)))))
+        (else (max (cdr lat))))))))
