1e73b7414d24b86357ba9ca9dc1bdc47843c33ad
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2019-08-09 11:08:02 +0300

    intented

--- max.scm
+++ max.scm
@@ -3,7 +3,7 @@
     (cond
       ((null? lat) (quote ()))
       (else
-       (cond
-        ((null? (cdr lat)) (car lat))
-        ((> (car lat) (car (cdr lat))) (max (cons (car lat) (cdr (cdr lat)))))
-        (else (max (cdr lat))))))))
+        (cond
+          ((null? (cdr lat)) (car lat))
+          ((> (car lat) (car (cdr lat))) (max (cons (car lat) (cdr (cdr lat)))))
+          (else (max (cdr lat))))))))
