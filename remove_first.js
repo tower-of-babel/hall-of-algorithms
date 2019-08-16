6471e6e4ec4ea9678e899ba5d86c051fbdee18e9
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2019-08-16 18:14:59 +0300

    renamed and eqaulity to identity

--- remove_first.js
+++ remove_first.js
@@ -1,11 +1,11 @@
-const remove_first = (x, a) => {
-    if (a.length === 0) {
+function remove (x, lat) {
+    if (lat.length === 0) {
 	return [];
     } else {
-	if (x == a[0]) {
-	    return a.slice(1);
+	if (lat[0] === x) { // TODO: should be == or ===
+	    return lat.slice(1);
 	} else {
-	    return [a[0], ...remove_first(x, a.slice(1))];
+	    return [lat[0], ...remove(x, lat.slice(1))];
 	}
     }
 }
